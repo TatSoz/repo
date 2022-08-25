328e1dbdabc9aa1b469073c6b4258bf21eac4299
tatyana <stv24@list.ru>
2022-08-25 18:20:25 +0300

    Ex010

---
+++ Ex010_MethodArray/Program.cs
@@ -0,0 +1,17 @@
+﻿/*  Имеется одномерный массив array из n элементов, требуется найти элемент массива равный find. */
+
+int[] array = {9, 3, 1, 7, 9, 2, 4};
+
+int n = array.Length;
+int find = 7;
+
+int index = 0;
+ while (index < n)
+ {
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+ }
