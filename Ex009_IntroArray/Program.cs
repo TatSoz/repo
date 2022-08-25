7219a79e19a1d91c7bfdef24e2caf33a4edf6669
tatyana <stv24@list.ru>
2022-08-25 17:50:16 +0300

    Ex009

---
+++ Ex009_IntroArray/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Найти максимальное с помощью массива */
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = {13,25,93,46,51,16,87,38,90};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
+
