c6e4eff341d92babeb9401d9130a66735d86f1b6
tatyana <stv24@list.ru>
2022-08-25 16:36:27 +0300

    Ex008

--- Ex008(2)/Program.cs
+++ Ex008(2)/Program.cs
@@ -20,10 +20,11 @@ int a3 = 39;
 int b3 = 74;
 int c3 = 53;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max( max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max( max1, max2, max3);
 
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
 Console.WriteLine(max);
 
